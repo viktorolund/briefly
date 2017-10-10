139de30b96f52322aaed8bc198612f1a48ca1cf0
Gabriel Seixas <salesseixasgabriel@gmail.com>
2017-10-09 21:09:09 -0300

    Added language support to french, english and portuguese #4

--- src/briefly.js
+++ src/briefly.js
@@ -1,68 +1,67 @@
-module.exports = class Briefly {
-	
+class Briefly {
 	static formatISO(date) {
 		// Handle "YYYY/MM/DD HH:mm:SS" Date formats for Safari
 		// If they are in that format we'll return "YYYY/MM/DDTHH:mm:SS"
-		if (date.split(" ").length > 1) return new Date(date.replace(" ", "T")).toISOString();
-		else return new Date(date).toISOString();
+		if (date.split(" ").length > 1) {
+			return new Date(date.replace(" ", "T")).toISOString();
+		} else {
+			return new Date(date).toISOString();
+		}
 	}
 	static leadZeroes(str) {
 		return str.length > 1 ? str : "0" + str;
 	}
-	
+
 	// DD/MM/YYYY ISO - 04-08-2016
 	static ddmmyyISO() {
 		let d = new Date();
-	
+
 		let day = d.getDate().toString();
 		let month = (d.getMonth() + 1).toString();
 		let year = d.getFullYear().toString();
-	
+
 		return `${year}-${this.leadZeroes(month)}-${this.leadZeroes(day)}`;
 	}
-	
-	// DD/MM/YYYY - 04/08/2016 
+
+	// DD/MM/YYYY - 04/08/2016
 	static ddmmyyyy(date) {
 		let d = new Date(date);
-	
+
 		let day = d.getDate().toString();
 		let month = (d.getMonth() + 1).toString();
 		let year = d.getFullYear().toString();
-	
+
 		return `${this.leadZeroes(day)}/${this.leadZeroes(month)}/${year}`;
 	}
-	
+
 	// DD/MM - 14/07
 	static ddmm(date) {
 		let d = new Date(date);
-	
+
 		let day = d.getDate().toString();
 		let month = (d.getMonth() + 1).toString();
-	
+
 		return `${this.leadZeroes(day)}/${this.leadZeroes(month)}`;
 	}
-	
+
 	// HH:MM - 16:08
 	static hhmm(date) {
 		let d = new Date(date);
-	
+
 		let hour = d.getHours().toString();
 		let minutes = d.getMinutes().toString();
-	
+
 		return `${this.leadZeroes(hour)}:${this.leadZeroes(minutes)}`;
 	}
-	
-	static weekDayFull(date) {
-		let weekDays = [
-			"domingo",
-			"segunda-feira",
-			"terça-feira",
-			"quarta-feira",
-			"quinta-feira",
-			"sexta-feira",
-			"sábado"
-		];
-	
-		return weekDays[new Date(date).getDay()];
+
+	static weekDayFull(date, lang) {
+		try {
+			const weekDays = require(`./langs/${lang}`);
+			return weekDays[new Date(date).getDay()];
+		} catch (err) {
+			throw new Error("Cannot find current language selected.");
+		}
 	}
-};
+}
+
+module.exports = Briefly;
