5b15056bea9f3be0e0f7bc503fd5c084b91ea21c
Viktor Ölund <viktor.olund@codemill.se>
2017-10-26 21:19:41 +0200

    test briefly methods

--- src/__tests__/briefly.test.js
+++ src/__tests__/briefly.test.js
@@ -1,4 +1,14 @@
-import { hhmm, leadZeroes } from '../briefly';
+import {
+    leadZeroes,
+    formatISO,
+    hhmm,
+    hhmmss,
+    hhmmddmm,
+    ddmm,
+    ddmmyyyy,
+    ddmmyyISO,
+    weekDayFull
+} from '../briefly';
 
 describe('briefly methods', () => {
     describe('method leadZeroes', () => {
@@ -11,6 +21,15 @@ describe('briefly methods', () => {
             expect(leadZeroes(inputData)).toEqual('foodcourt');
         });
     });
+    describe('method formatISO', () => {
+        let time;
+        beforeEach(() => {
+            time = '2017-10-26';
+        });
+        it('should return the time 2017-10-26T00:00:00.000Z', () => {
+            expect(formatISO(time)).toEqual('2017-10-26T00:00:00.000Z');
+        });
+    });
     describe('method hhmm', () => {
         let time;
         beforeEach(() => {
@@ -18,6 +37,60 @@ describe('briefly methods', () => {
         });
         it('should return the time 20:24', () => {
             expect(hhmm(time)).toEqual('20:24');
+        });
+    });
+    describe('method hhmmss', () => {
+        let time;
+        beforeEach(() => {
+            time = 'Thu Oct 26 2017 20:24:52 GMT+0200 (CEST)';
+        });
+        it('should return the time 20:24:52', () => {
+            expect(hhmmss(time)).toEqual('20:24:52');
+        });
+    });
+    describe('method hhmmddmm', () => {
+        let time;
+        beforeEach(() => {
+            time = 'Thu Oct 26 2017 20:24:52 GMT+0200 (CEST)';
+        });
+        it('should return the time 20:24', () => {
+            expect(hhmmddmm(time)).toEqual('20:24 26/10');
+        });
+    });
+    describe('method ddmm', () => {
+        let time;
+        beforeEach(() => {
+            time = 'Thu Oct 26 2017 20:24:52 GMT+0200 (CEST)';
+        });
+        it('should return the time 26/10', () => {
+            expect(ddmm(time)).toEqual('26/10');
+        });
+    });
+    describe('method ddmmyyyy', () => {
+        let time;
+        beforeEach(() => {
+            time = 'Thu Oct 26 2017 20:24:52 GMT+0200 (CEST)';
+        });
+        it('should return the time 26/10/2017', () => {
+            expect(ddmmyyyy(time)).toEqual('26/10/2017');
+        });
+    });
+    describe('method ddmmyyISO', () => {
+        let time;
+        beforeEach(() => {
+            time = 'Thu Oct 26 2017 20:24:52 GMT+0200 (CEST)';
+        });
+        it('should return the time 2017-10-26', () => {
+            expect(ddmmyyISO(time)).toEqual('2017-10-26');
+        });
+    });
+    describe('method weekDayFull', () => {
+        let time;
+        beforeEach(() => {
+            time = 'Thu Oct 26 2017 20:24:52 GMT+0200 (CEST)';
+        });
+        it('should return the time jeudi', () => {
+            expect(weekDayFull(time, 'fr')).toEqual('jeudi');
         });
     });
 });
